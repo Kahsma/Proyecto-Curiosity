f0f1cf9bdb4173587e23fefb395393d7346e4386
Camilo Jose Martinez <105135927+Kahsma@users.noreply.github.com>
2023-02-15 00:01:07 +0000

    pruebas

--- shell.cxx
+++ shell.cxx
@@ -416,6 +416,10 @@ void shell::rutaMasLarga()
 }
 void shell::limpiar(){
   #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+  if (system ("CLS")==-1)
+    {
+      cout<<"ERROR"<<endl;
+    }
    //define something for Windows (32-bit and 64-bit, this part is common)
    #ifdef _WIN64
     if (system ("CLS")==-1)
@@ -432,16 +436,10 @@ void shell::limpiar(){
    #endif
 #elif __APPLE__
     #include <TargetConditionals.h>
-    #if TARGET_IPHONE_SIMULATOR
-         // iOS, tvOS, or watchOS Simulator
-    #elif TARGET_OS_MACCATALYST
-         // Mac's Catalyst (ports iOS API into Mac, like UIKit).
-    #elif TARGET_OS_IPHONE
-        // iOS, tvOS, or watchOS device
-    #elif TARGET_OS_MAC
-        // Other kinds of Apple platforms
-    #else
-    #   error "Unknown Apple platform"
+    if (system ("clear")==-1)
+    {
+      cout<<"ERROR"<<endl;
+    }
     #endif
 #elif __ANDROID__
     // Below __linux__ check should be enough to handle Android,
