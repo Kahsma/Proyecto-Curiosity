82a68e88ec712cdbb2e00de61d8595788bff5998
Camilo Jose Martinez <cajomapa@gmail.com>
2023-04-25 17:49:52 -0500

    hola

--- Implementacion/Curiosity.hxx
+++ Implementacion/Curiosity.hxx
@@ -1,15 +1,13 @@
-#include"Curiosity.h"
-#include<list>
+#include "Curiosity.h"
+#include <list>
 using namespace std;
 // Constructor
 Curiosity::Curiosity()
 {
-  
 }
 
-// Destructor 
+// Destructor
 Curiosity::~Curiosity() {}
-
 
 // Funciones del TAD Curiosity
 // Funciones Queue de Comandos
@@ -18,44 +16,43 @@ queue<Comandos> Curiosity::get_queue_de_comandos()
   return ColaDeComandos;
 }
 
-void Curiosity::set_queue_de_comandos(queue<Comandos>ColaDeComandos)
+void Curiosity::set_queue_de_comandos(queue<Comandos> ColaDeComandos)
 {
   this->ColaDeComandos = ColaDeComandos;
 }
 
-list<Elementos> Curiosity::get_lista_de_elementos ()
+list<Elementos> Curiosity::get_lista_de_elementos()
 {
   return listaDeElementos;
 }
 
 void Curiosity::set_lista_de_elementos(list<Elementos> lista_elementos)
 {
-  listaDeElementos=lista_elementos;
+  listaDeElementos = lista_elementos;
 }
 
 // Funciones comandos
 bool Curiosity::agregar_comando(Comandos comando)
 {
-  bool resultado_agregar_comando=false;
+  bool resultado_agregar_comando = false;
   ColaDeComandos.push(comando);
-  resultado_agregar_comando=true;
+  resultado_agregar_comando = true;
   return resultado_agregar_comando;
 }
 
 bool Curiosity::borrar_comando(Comandos comando)
 {
   bool resultado_borrar_comando = false;
-  
-  return resultado_borrar_comando; 
-}
 
+  return resultado_borrar_comando;
+}
 
 // Funciones elemento
 bool Curiosity::agregar_elemento(Elementos elementos)
 {
   bool resultado_agregar_elemento = false;
   listaDeElementos.push_back(elementos);
-  resultado_agregar_elemento=true;
+  resultado_agregar_elemento = true;
   return resultado_agregar_elemento;
 }
 
